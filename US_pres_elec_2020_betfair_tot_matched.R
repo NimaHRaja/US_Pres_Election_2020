6940247eb590c294f95c72bf4891ff28f3b4a5f1
NimaHRaja <nimaradja@gmail.com>
2020-11-03 18:57:45 +0000

    total_matched_last_hours added

--- US_pres_elec_2020_betfair_tot_matched.R
+++ US_pres_elec_2020_betfair_tot_matched.R
@@ -38,3 +38,16 @@ next_pres_odds %>%
     ylab("Total Matched (£)") +
     ggtitle("Next US President Market - Total Matched (Betfair)")
 # dev.off()
+
+
+# png("US_pres_elec_2020_betfair_tot_matched-2.png", width = 800, height = 600)
+next_pres_odds %>% 
+    filter(Date > as.POSIXct('2020/11/02')) %>%
+    ggplot(aes(x = Date, y = totalMatched)) + 
+    geom_point(colour = "blue") + 
+    scale_y_continuous(
+                       labels = unit_format(unit = "M", scale = 1e-6)) +
+    ylab("Total Matched (£)") +
+    xlab("time (GMT)") +
+    ggtitle("Next US President Market - Total Matched (Betfair)")
+# dev.off()
